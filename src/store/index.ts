fa9ae5325c6dccece5eaa5df75f98142a7fda1dc
Eliran Levi <13661726+eliranlevi@users.noreply.github.com>
2020-05-17 00:19:19 -0700

    Add store base code

---
+++ src/store/index.ts
@@ -0,0 +1,11 @@
+import ReduxThunk from "redux-thunk";
+import { createStore, applyMiddleware } from "redux";
+import rootReducer from "../reducers";
+
+const store = createStore(
+  rootReducer,
+  {},
+  applyMiddleware(ReduxThunk)
+);
+
+export default store;
