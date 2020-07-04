a596cc3e2080b7ee61204ca6d897e4fbc6aeb97b
Eliran Levi <13661726+eliranlevi@users.noreply.github.com>
2020-07-04 15:26:45 -0700

    Enhance GistList look and feel
    - Add refresh button
    - Add header
    - Use scrollable list
    - Add [secret] badge to secret gists

--- src/components/GistList.tsx
+++ src/components/GistList.tsx
@@ -1,10 +1,10 @@
 import React from "react";
-import { Icon, Button, Text } from "react-native-elements";
+import { FlatList, View } from "react-native";
+import { Button, Icon, Text } from "react-native-elements";
 import { GistListProps } from "../containers/GistListContainer";
 import { Gist } from "../interfaces/common";
-import GistItem from "./GistItem";
-import { View } from "react-native";
 import StylesConsts from "../styles/consts";
+import GistItem from "./GistItem";
 
 const GistList = ({
   username,
@@ -15,12 +15,38 @@ const GistList = ({
     <>
       {gists.length
         ? (
-          gists.map((gist: Gist): JSX.Element => (
-            <GistItem key={gist.id} gist={gist} />
-          ))
+          <View>
+            <View style={{
+              paddingStart: 25,
+              paddingEnd: 10,
+              paddingVertical: 10,
+              marginBottom: 5,
+              display: "flex",
+              flexDirection: "row",
+              justifyContent: "space-between",
+              alignItems: "center",
+              backgroundColor: StylesConsts.bgLight,
+            }}>
+              <Text style={{ fontSize: 16, fontWeight: "500" }}>Showing gists for {username}</Text>
+              <Button icon={<Icon name="refresh" />} onPress={getGists} type="clear" />
+            </View>
+            <FlatList
+              style={{
+                height: "90%",
+                marginEnd: StylesConsts.rootMarginSide,
+                marginStart: StylesConsts.rootMarginSide,
+              }}
+              keyExtractor={(item, index): string => index.toString()}
+              data={gists}
+              renderItem={({ item: gist }: { item: Gist }): JSX.Element => (
+                <GistItem key={gist.id} gist={gist} />
+              )}
+            />
+          </View>
         )
         : (
           <View style={{ alignItems: "center", display: "flex", flexDirection: "column", justifyContent: "center", height: "100%" }}>
+            <Icon name="folder-open" size={100} style={{ marginBottom: 50 }} color={StylesConsts.textColor} />
             <Text style={{ color: StylesConsts.textColor }}>
               No Gists here.
             </Text>
